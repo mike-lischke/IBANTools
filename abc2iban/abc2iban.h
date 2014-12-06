dbb12042737051a5830ff584d5c61a98ba210856
Mike Lischke <mike@lischke-online.de>
2014-12-06 15:03:20 +0100

    Initial Commit

---
+++ abc2iban/abc2iban.h
@@ -0,0 +1,19 @@
+//
+//  abc2iban.h
+//  abc2iban
+//
+//  Created by Mike Lischke on 06.12.14.
+//  Copyright (c) 2014 Mike Lischke. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for abc2iban.
+FOUNDATION_EXPORT double abc2ibanVersionNumber;
+
+//! Project version string for abc2iban.
+FOUNDATION_EXPORT const unsigned char abc2ibanVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <abc2iban/PublicHeader.h>
+
+
